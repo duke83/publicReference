a5c99fd84fe20d4189c5e8d90e17e560e1e1588d
Kent Merrell <kmerrell@nerium.com>
2015-03-11 11:27:47 -0500

    props_1_notValid:     No address objects, only address properties for a single address in a parent scope.     Service returns not valid.     Service returns suggested address.

--- app/validAddress/validAddressDirective.js
+++ app/validAddress/validAddressDirective.js
@@ -10,19 +10,25 @@ var app = angular.module('app', []);
 
                 templateUrl: "../../StaticUiDev/mocks/Nerium.AdoManager/Views/Renderings/Modules/ValidAddressDirective.html",
                 controller: function ($scope) {
-                    $scope.editMode = false;
-
 
                     $scope.SuggestedAddress = null;
 
                     $scope.GetSuggestedAddress = function () {
-                        $scope.ParentAddressFailsValidation = true;
+
+                        $scope.ParentAddressFailsValidation = !$scope.mockServiceReturn.isValid;
+
+                        //$scope.address1 = $scope.mockServiceReturn.AutoOrderReturnAddressDto.Address1
+                        //$scope.address2 = $scope.mockServiceReturn.AutoOrderReturnAddressDto.Address2
+                        //$scope.city = $scope.mockServiceReturn.AutoOrderReturnAddressDto.City
+                        //$scope.state = $scope.mockServiceReturn.AutoOrderReturnAddressDto.State
+                        //$scope.zip = $scope.mockServiceReturn.AutoOrderReturnAddressDto.Zip
+
                         $scope.SuggestedAddress = {
-                            "address1": "571 Garner Rd",
-                            "address2": "",
-                            "city": "CJ",
-                            "state": "OR",
-                            "zip": "92232"
+                            "address1": $scope.mockServiceReturn.AutoOrderReturnAddressDto.Address1,
+                            "address2": $scope.mockServiceReturn.AutoOrderReturnAddressDto.Address2,
+                            "city": $scope.mockServiceReturn.AutoOrderReturnAddressDto.City,
+                            "state": $scope.mockServiceReturn.AutoOrderReturnAddressDto.State,
+                            "zip": $scope.mockServiceReturn.AutoOrderReturnAddressDto.Zip
                         };
                     }
                 },
@@ -39,36 +45,54 @@ var app = angular.module('app', []);
                         "zip": attrs.zip
                     };
 
-                    scp.selectSuggested = function () {
-                        scp.address1 = scp.SuggestedAddress.address1;
-                        scp.address2 = scp.SuggestedAddress.address2;
-                        scp.city = scp.SuggestedAddress.city;
-                        scp.state = scp.SuggestedAddress.state;
-                        scp.zip = scp.SuggestedAddress.zip;
+                    scp.selectAddress = function (source) { // source is 'suggested' | 'override'
+                        scp.userApproved=true;
+                        if (source === 'suggested') {
+                            scp.address1 = scp.SuggestedAddress.address1;
+                            scp.address2 = scp.SuggestedAddress.address2;
+                            scp.city = scp.SuggestedAddress.city;
+                            scp.state = scp.SuggestedAddress.state;
+                            scp.zip = scp.SuggestedAddress.zip;
+                        }
+                        if (source === 'override') {
+                            scp.address1 = scp.CopyOfParentAddress.address1;
+                            scp.address2 = scp.CopyOfParentAddress.address2;
+                            scp.city = scp.CopyOfParentAddress.city;
+                            scp.state = scp.CopyOfParentAddress.state;
+                            scp.zip = scp.CopyOfParentAddress.zip;
+                        }
                         //todo: update attrs
-                    }
-
-                    scp.selectOriginal = function () {
-                        scp.address1 = scp.CopyOfParentAddress.address1;
-                        scp.address2 = scp.CopyOfParentAddress.address2;
-                        scp.city = scp.CopyOfParentAddress.city;
-                        scp.state = scp.CopyOfParentAddress.state;
-                        scp.zip = scp.CopyOfParentAddress.zip;
-                        //tod: update attrs
                     }
 
                     scp.showMessage = function (message) {
                         alert(scp.OriginalAddress.address1);
                     }
-
                 }
-
             }
         })
 }());
 
 angular.module('app')
-    .controller('addresscontroller', ['$scope', function ($scope) {
+    .controller('props_1_notValid_ctrl', ['$scope', function ($scope) {
+        $scope.myPrimaryAddress = {
+            address1: "580 Garner Rd",
+            address2: "yyy",
+            city: "CJ",
+            state: "OR",
+            zip: "92232"
+        };
+
+        $scope.mockServiceReturn = {
+            "isValid": false,
+            "AutoOrderReturnAddressDto": {
+                "Address1": "590 Garnet Rd",
+                "Address2": "",
+                "City": "Cave Junction",
+                "State": "OR",
+                "Zip": "97523-4342"
+            }
+        }
+
         $scope.address1 = "570 Garner Rd";
         $scope.address2 = "xxx";
         $scope.city = "CJ";
